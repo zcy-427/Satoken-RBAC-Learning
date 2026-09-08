58a195ec7092486d16c12ad0b896e75910a287b8
Amad1eus <181839446+zcy-427@users.noreply.github.com>
2026-09-08 20:59:32 +0800

    feat: scaffold Spring Boot backend application

---
+++ backend/src/test/java/io/github/zcy427/rbac/SatokenRbacBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zcy427.rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatokenRbacBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
